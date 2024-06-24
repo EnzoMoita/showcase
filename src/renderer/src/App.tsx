cfe6ffd5eea4277ceb13b5204844114551a131f6
Enzo Salomão <enzosadourado@hotmail.com>
2024-06-24 08:02:25 -0300

    ConfigEsLintAndTypeScript

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,5 @@
 import './styles/global.css'
 
 export function App() {
-
-  return <h1 className='text-violet-400 font-bold text-4xl'>Hello Enzo</h1>
-  
+  return <h1 className="text-violet-400 font-bold text-4xl">Hello Enzo</h1>
 }
-

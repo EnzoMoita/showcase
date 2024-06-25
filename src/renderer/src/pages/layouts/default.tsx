38d1a8c82fe58a84fca6f0cdde9ba9b4d3be8360
Enzo Salomão <enzosadourado@hotmail.com>
2024-06-25 18:23:35 -0300

    AddAnimationSidebar

--- src/renderer/src/pages/layouts/default.tsx
+++ src/renderer/src/pages/layouts/default.tsx
@@ -1,16 +1,24 @@
 import { Sidebar } from '../../components/SideBar'
 import { Header } from '../../components/Header'
 import { Outlet } from 'react-router-dom'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { useState } from 'react'
 
 export function Default() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
   return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100 flex">
+    <Collapsible.Root
+      defaultOpen
+      onOpenChange={setIsSidebarOpen}
+      className="h-screen w-screen bg-rotion-900 text-rotion-100 flex"
+    >
       <Sidebar />
       <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
+        <Header isSidebarOpen={isSidebarOpen} />
 
         <Outlet />
       </div>
-    </div>
+    </Collapsible.Root>
   )
 }
